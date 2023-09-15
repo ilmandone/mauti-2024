2073743fc2a0b017fd7862c29d6a1ef1804165ea
Andrea Mandini <a.mandini@mauti.it>
2023-09-15 17:53:09 +0200

    chore: add alias for styles

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@styles': fileURLToPath(new URL('./src/styles', import.meta.url)),
     }
   }
 })
