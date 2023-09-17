639ad8f88dbe48fbbe3065bc7cf67a4483b4389c
Andrea Mandini <a.mandini@mauti.it>
2023-09-17 11:02:27 +0200

    feat: add hello svg and use it SHello.vue

--- src/components/sections/SHello.vue
+++ src/components/sections/SHello.vue
@@ -3,12 +3,35 @@
 </script>
 
 <template>
-	<h1>
-		Hello
-	</h1>
+	<div class='hello'>
+		<div class='hello__picture' aria-hidden='true'>
+			<svg class='hello-svg' viewBox="0 0 1297.56 500.59">
+				<use xlink:href='/vectors/hello.svg#hello'></use>
+			</svg>
+		</div>
+		<div class='hello__text'>
+			<h1>Hello</h1>
+		</div>
+
+	</div>
+
 	<p>I am a front end engineer with a deep love for UX/UI design and 3D</p>
 </template>
 
-<style scoped>
+<style scoped lang='scss'>
+
+.hello{
+	width: 100vw;
+
+	&__text h1 {
+		color: red;
+		visibility: hidden;
+		font-size: 0;
+	}
+}
+
+.hello-svg {
+	fill: var(--color-emphasize);
+}
 
 </style>
