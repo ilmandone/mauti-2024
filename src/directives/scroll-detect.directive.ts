9021ef92d02c95136e9ce2cbce13b3564bea57b4
Andrea Mandini <a.mandini@mauti.it>
2023-09-29 21:53:00 +0200

    refactoring: remove unused

--- src/directives/scroll-detect.directive.ts
+++ src/directives/scroll-detect.directive.ts
@@ -1,36 +1,26 @@
 import type { Directive } from 'vue'
 
-/*function onPointerDown() {
-	window.addEventListener('pointerup', onPointerUp)
-	console.log(this)
-}
-
-function onPointerUp() {
-	console.log('UP')
-	window.removeEventListener('pointerup', onPointerUp)
-}*/
-
 type ScrollDirective = Directive & {
 	isTouch: boolean
 	cbFn: (v: number) => void
 
 	startY: number
-	deltaY: number
-	onPointerDown: (e:PointerEvent) => void
-	onPointerUp: (e:PointerEvent) => void
-	onPointerMove: (e:PointerEvent) => void
-	onWheel: (e:WheelEvent) => void
+
+	onPointerDown: (e: PointerEvent) => void
+	onPointerUp: (e: PointerEvent) => void
+	onPointerMove: (e: PointerEvent) => void
+	onWheel: (e: WheelEvent) => void
 }
 
 export const ScrollDetectDirective: ScrollDirective = {
 
 	isTouch: false,
-	cbFn(v: number) {},
+	cbFn(v: number) {
+	},
 
 	startY: 0,
-	deltaY: 0,
 
-	onWheel(e){
+	onWheel(e) {
 		ScrollDetectDirective.cbFn(e.deltaY)
 	},
 
@@ -39,7 +29,7 @@ export const ScrollDetectDirective: ScrollDirective = {
 		window.addEventListener('pointermove', ScrollDetectDirective.onPointerMove)
 	},
 
-	onPointerMove(e){
+	onPointerMove(e) {
 		ScrollDetectDirective.cbFn(ScrollDetectDirective.startY - e.clientY)
 	},
 
@@ -54,18 +44,18 @@ export const ScrollDetectDirective: ScrollDirective = {
 		ScrollDetectDirective.cbFn = binding.value.cbFn
 
 		// Detect touch device
-		ScrollDetectDirective.isTouch =  navigator.maxTouchPoints > 0 || 'ontouchstart' in window
+		ScrollDetectDirective.isTouch = navigator.maxTouchPoints > 0 || 'ontouchstart' in window
 	},
 
 	mounted(el: HTMLElement) {
-		if(!ScrollDetectDirective.isTouch)
+		if (!ScrollDetectDirective.isTouch)
 			window.addEventListener('pointerdown', ScrollDetectDirective.onPointerDown)
 
 		window.addEventListener('wheel', ScrollDetectDirective.onWheel)
 	},
 
 	unmounted(el: HTMLElement) {
-		if(!ScrollDetectDirective.isTouch)
+		if (!ScrollDetectDirective.isTouch)
 			window.removeEventListener('pointerdown', ScrollDetectDirective.onPointerDown)
 
 		window.removeEventListener('wheel', ScrollDetectDirective.onWheel)
