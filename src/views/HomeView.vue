3760aad79f677b0248a3136d9e5f425865fb0139
Andrea Mandini <a.mandini@mauti.it>
2023-09-29 22:13:24 +0200

    feat: set styles and wrapper

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,26 +12,40 @@ const scrollValue = ref<number>(0)
 
 const updateScroll = (v:number) => {
 	console.log(v)
+	scrollValue.value -= v
 }
 
 </script>
 
 <template>
+	<div class='wrapper'>
 
-	<main v-scroll-detect='{cbFn: updateScroll}'>
-		<SHello />
-		<SBrief />
-		<SWhatIDo />
-		<SUpToNow />
-		<SContacts />
-	</main>
+		<main v-scroll-detect='{cbFn: updateScroll}' :style='{transform: `translate3d(0, ${scrollValue}px, 0)`}'>
+			<SHello />
+			<SBrief />
+			<SWhatIDo />
+			<SUpToNow />
+			<SContacts />
+		</main>
 
+	</div>
 
 </template>
 
 <style lang='scss' scoped>
-main {
-	overflow: hidden;
+
+.wrapper {
+	position: fixed;
+	width: 100vw;
 	height: 100vh;
+	user-select: none;
+	box-sizing: border-box;
+}
+main {
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	// height: 100vh;
 }
 </style>
