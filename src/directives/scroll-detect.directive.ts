9ca1bac2e9fffea207a22678b6873609742b1857
Andrea Mandini <a.mandini@mauti.it>
2023-09-29 21:51:37 +0200

    feat: directive for drag or wheel scroll detection

--- src/directives/scroll-detect.directive.ts
+++ src/directives/scroll-detect.directive.ts
@@ -1,7 +1,73 @@
 import type { Directive } from 'vue'
 
-export const ScrollDetectDirective: Directive = {
-	created(el: HTMLElement) {
-		console.log(el)
+/*function onPointerDown() {
+	window.addEventListener('pointerup', onPointerUp)
+	console.log(this)
+}
+
+function onPointerUp() {
+	console.log('UP')
+	window.removeEventListener('pointerup', onPointerUp)
+}*/
+
+type ScrollDirective = Directive & {
+	isTouch: boolean
+	cbFn: (v: number) => void
+
+	startY: number
+	deltaY: number
+	onPointerDown: (e:PointerEvent) => void
+	onPointerUp: (e:PointerEvent) => void
+	onPointerMove: (e:PointerEvent) => void
+	onWheel: (e:WheelEvent) => void
+}
+
+export const ScrollDetectDirective: ScrollDirective = {
+
+	isTouch: false,
+	cbFn(v: number) {},
+
+	startY: 0,
+	deltaY: 0,
+
+	onWheel(e){
+		ScrollDetectDirective.cbFn(e.deltaY)
+	},
+
+	onPointerDown(e) {
+		window.addEventListener('pointerup', ScrollDetectDirective.onPointerUp)
+		window.addEventListener('pointermove', ScrollDetectDirective.onPointerMove)
+	},
+
+	onPointerMove(e){
+		ScrollDetectDirective.cbFn(ScrollDetectDirective.startY - e.clientY)
+	},
+
+	onPointerUp(e) {
+		window.removeEventListener('pointerup', ScrollDetectDirective.onPointerUp)
+		window.removeEventListener('pointermove', ScrollDetectDirective.onPointerMove)
+	},
+
+	created(el: HTMLElement, binding) {
+
+		// Register the cb function
+		ScrollDetectDirective.cbFn = binding.value.cbFn
+
+		// Detect touch device
+		ScrollDetectDirective.isTouch =  navigator.maxTouchPoints > 0 || 'ontouchstart' in window
+	},
+
+	mounted(el: HTMLElement) {
+		if(!ScrollDetectDirective.isTouch)
+			window.addEventListener('pointerdown', ScrollDetectDirective.onPointerDown)
+
+		window.addEventListener('wheel', ScrollDetectDirective.onWheel)
+	},
+
+	unmounted(el: HTMLElement) {
+		if(!ScrollDetectDirective.isTouch)
+			window.removeEventListener('pointerdown', ScrollDetectDirective.onPointerDown)
+
+		window.removeEventListener('wheel', ScrollDetectDirective.onWheel)
 	}
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,18 +4,34 @@ import SBrief from '@components/sections/S-Brief.vue'
 import SWhatIDo from '@components/sections/S-WhatIDo.vue'
 import SUpToNow from '@components/sections/S-UpToNow.vue'
 import SContacts from '@components/sections/S-Contacts.vue'
+
+import {ScrollDetectDirective as vScrollDetect} from '@/directives/scroll-detect.directive'
+import { ref } from 'vue'
+
+const scrollValue = ref<number>(0)
+
+const updateScroll = (v:number) => {
+	console.log(v)
+}
+
 </script>
 
 <template>
 
-	<SHello />
-	<SBrief />
-	<SWhatIDo />
-	<SUpToNow />
-	<SContacts />
+	<main v-scroll-detect='{cbFn: updateScroll}'>
+		<SHello />
+		<SBrief />
+		<SWhatIDo />
+		<SUpToNow />
+		<SContacts />
+	</main>
+
 
 </template>
 
 <style lang='scss' scoped>
-
+main {
+	overflow: hidden;
+	height: 100vh;
+}
 </style>
