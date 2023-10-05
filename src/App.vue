e17766a14acb79e3a0ed3c710b207984eeef38e7
Andrea Mandini <a.mandini@mauti.it>
2023-10-05 17:43:05 +0200

    feat: add scroll animation time css var

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+    document.documentElement.style.setProperty(
+        '--main-scroll-animation-time',
+        window.navigator.maxTouchPoints > 0 ? '600ms' : '1200ms'
+    )
+})
 </script>
 
 <template>
-  <RouterView />
+    <RouterView />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
