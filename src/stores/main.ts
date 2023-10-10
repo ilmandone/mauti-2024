9390f907696ba1f2ac50f67fa85e915cac269ddc
Andrea Mandini <a.mandini@mauti.it>
2023-10-10 22:29:15 +0200

    feat: add theme to state manager

--- src/stores/main.ts
+++ src/stores/main.ts
@@ -1,12 +1,19 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
+export type Themes = 'light' | 'dark'
+
 export const useMainStore = defineStore('main', () => {
     // Touch state
     const isTouch = ref(false)
+    const theme = ref<Themes>('light')
     function setIsTouch(v: boolean) {
         isTouch.value = v
     }
 
-    return { isTouch, setIsTouch }
+    function setTheme(v: Themes) {
+        theme.value = v
+    }
+
+    return { isTouch, setIsTouch, theme, setTheme }
 })
