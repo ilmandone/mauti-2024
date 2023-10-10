b080f8bee69826e154b7b575d97b2ee100873d7e
Andrea Mandini <a.mandini@mauti.it>
2023-10-10 22:55:12 +0200

    feat: set initial value for theme

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import { useMainStore } from '@stores/main'
+import { h, onMounted } from 'vue'
+
+const store = useMainStore()
+const { setTheme } = store
+
+onMounted(() => {
+    const hour = new Date().getHours()
+    setTheme(hour > 6 && hour < 20 ? 'light' : 'dark')
+})
 </script>
 
 <template>
