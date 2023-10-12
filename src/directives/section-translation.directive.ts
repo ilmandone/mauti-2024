079c79c2058a549c0e7bcf66bbc66113b82461f4
Andrea Mandini <a.mandini@mauti.it>
2023-10-12 20:47:56 +0200

    refactoring: remove comments and console.log

--- src/directives/section-translation.directive.ts
+++ src/directives/section-translation.directive.ts
@@ -21,22 +21,11 @@ export const SectionTranslationDirective: Directive = {
         const elHeight = el.offsetHeight
         const scrollValue: number = binding.value.scrollValue
         const topPosition = el.offsetTop
-        /*console.log('***********************************')
-        console.log('scroll', scrollValue)
-        console.log('height', elHeight)
-        console.log('top', topPosition)
-        console.log('scroll + height', scrollValue + elHeight)
-        console.log('top + height', topPosition + elHeight)
-        console.log('mainHeight', mainHeight)
-        console.log('***********************************')
-        console.log(scrollValue + mainHeight)
-        console.log(mainHeight - (topPosition + elHeight + mainHeight * el.multiplier))*/
+
         if (scrollValue + mainHeight < mainHeight - (topPosition + elHeight + mainHeight * el.multiplier)) {
-            console.log('UFFA')
             el.multiplier += 1
             el.style.transform = `translateY(${mainHeight * el.multiplier}px)`
         } else if (mainHeight - scrollValue < topPosition + elHeight + mainHeight * el.multiplier) {
-            console.log('GNUFFA')
             el.multiplier -= 1
             el.style.transform = `translateY(${-mainHeight * el.multiplier * -1}px)`
         }
