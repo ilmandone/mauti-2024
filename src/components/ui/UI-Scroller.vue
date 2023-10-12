08885102eda304e57fb98d00d1640b009c6d9e2e
Andrea Mandini <a.mandini@mauti.it>
2023-10-12 22:14:30 +0200

    feat(scroll-progress): create scroller component

---
+++ src/components/ui/UI-Scroller.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="scroller" aria-hidden="true"></div>
+</template>
+
+<style scoped lang="scss">
+.scroller {
+    position: fixed;
+    top: 0;
+    right: 0;
+    background-color: var(--color-main);
+
+    pointer-events: none;
+}
+</style>
