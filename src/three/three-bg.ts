a31de253134c4f938944070950a2d56e632c3504
Andrea Mandini <a.mandini@mauti.it>
2023-10-16 19:10:55 +0200

    feat(background): resize and plane

--- src/three/three-bg.ts
+++ src/three/three-bg.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import debounce from 'lodash.debounce'
 
 export class ThreeBackground {
     private _scene!: THREE.Scene
@@ -11,18 +12,50 @@ export class ThreeBackground {
         this._container = container
     }
 
+    /**
+     * Init THREE and scene elements
+     * @private
+     */
     private _init() {
-        this._windowRatio = window.innerWidth / window.innerHeight
-        this._scene = new THREE.Scene()
-        this._camera = new THREE.PerspectiveCamera(75, this._windowRatio, 0.1, 1000)
+        const planeSize = 1.55
+        const cameraDist = 1
 
-        const rend: THREE.WebGLRenderer = new THREE.WebGLRenderer()
-        rend.setSize(window.innerWidth, window.innerHeight)
-        this._container.appendChild(rend.domElement)
+        const _windowRatio = window.innerWidth / window.innerHeight
+        const _scene: THREE.Scene = new THREE.Scene()
+        const _camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, this._windowRatio, 0.1, 1000)
+        _camera.position.z = cameraDist
 
-        this._renderer = rend
+        const _renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
+        _renderer.setSize(window.innerWidth, window.innerHeight)
+
+        const g = new THREE.PlaneGeometry(planeSize, planeSize)
+        const m = new THREE.MeshBasicMaterial({ color: 0xff0000, side: THREE.DoubleSide })
+        const plane = new THREE.Mesh(g, m)
+        _scene.add(plane)
+
+        return { _camera, _renderer, _scene, _windowRatio }
     }
 
+    /**
+     * Update render and camera to match viewport
+     * @private
+     */
+    private _setViewPort() {
+        this._renderer.setSize(window.innerWidth, window.innerHeight)
+        this._camera.aspect = window.innerWidth / window.innerHeight
+        this._camera.fov = Math.atan(window.innerHeight / 2 / this._camera.position.z) * 2 * THREE.MathUtils.RAD2DEG
+    }
+
+    /**
+     * Window resize
+     * @private
+     */
+    private _resize = debounce(this._setViewPort, 50)
+
+    /**
+     * Start render loop
+     * @private
+     */
     private _start() {
         window.requestAnimationFrame(this._start.bind(this))
         this._renderer.render(this._scene, this._camera)
@@ -31,9 +64,13 @@ export class ThreeBackground {
     //#region Public
 
     public start() {
-        this._init()
+        Object.assign(this, this._init())
+        this._container.appendChild(this._renderer.domElement)
+
+        this._setViewPort()
+        window.addEventListener('resize', this._resize.bind(this))
+
         this._start()
     }
-
     //#endregion
 }
