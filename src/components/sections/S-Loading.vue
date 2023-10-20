7108a08a73c4ffe0402b0c22f4b923f80fc2df9c
Andrea Mandini <a.mandini@mauti.it>
2023-10-20 19:04:55 +0200

    feat(loading-screen): add progress and set value animation

--- src/components/sections/S-Loading.vue
+++ src/components/sections/S-Loading.vue
@@ -7,33 +7,60 @@ const emits = defineEmits<{
 }>()
 
 const visible = ref<boolean>(true)
+const displayed = ref<number>(0)
+const interval = ref<number | null>(null)
 
-watch(
+const startInterval = () => {
+    interval.value = window.setInterval(handleInterval, 30)
+}
+
+const stopInterval = () => {
+    window.clearInterval(interval.value as number)
+    interval.value = null
+}
+
+const handleInterval = (): void => {
+    if (displayed.value < props.progress) {
+        displayed.value += Math.round((props.progress - displayed.value) / 5)
+    }
+
+    if (displayed.value > 95 && props.progress === 100) {
+        displayed.value = 100
+        stopInterval()
+        console.log('HIDE LOADING')
+    }
+}
+
+/*watch(
     () => props.progress,
     (v) => {
         if (v === 100) {
-            console.log('HIDE LOADING')
+
             // TODO: al termine dell'animazione di uscita
-            visible.value = false
+            // visible.value = false
         }
     }
-)
+)*/
 
 onMounted(() => {
     // TODO Al termine dell'animazione di ingresso
     window.setTimeout(() => {
         console.log('EMIT START LOADING')
+        startInterval()
         emits('startLoading', true)
     }, 1000)
 })
 </script>
 
 <template>
-    <div v-if="visible" class="loading-wrapper"></div>
+    <div v-if="visible" class="loading-wrapper">
+        <div class="loading">{{ displayed }}</div>
+    </div>
 </template>
 
 <style scoped lang="scss">
 @use '@styles/utils';
+@use '@styles/typo';
 
 .loading-wrapper {
     position: absolute;
@@ -42,8 +69,19 @@ onMounted(() => {
     width: 100vw;
     height: 100vh;
 
-    background-color: lavender;
+    background-color: var(--color-emphasize);
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
     @include utils.zIndex('loader');
+
+    .loading {
+        @include typo.headers(10vw, var(--background-color));
+        font-weight: 700;
+
+        pointer-events: none;
+    }
 }
 </style>
