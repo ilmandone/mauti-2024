92f41add4cbfa6cd32eeee87e12c50c4d5a78e4a
Andrea Mandini <a.mandini@mauti.it>
2023-11-11 18:02:02 +0100

    feat: add animation for hello section with common

--- src/components/sections/s-hello.vue
+++ src/components/sections/s-hello.vue
@@ -2,16 +2,20 @@
 import { inject, onMounted, ref } from 'vue'
 import { ADD_TO_OBSERVER, type IAddToObserver } from '@components/renderless/r-int-observer'
 
-const el = ref()
+const hello = ref<HTMLElement>()
+const helloSub = ref<HTMLElement>()
 
 const addToObserver = inject(ADD_TO_OBSERVER) as IAddToObserver
 
-onMounted(() => addToObserver(el.value))
+onMounted(() => {
+    addToObserver(hello.value as HTMLElement)
+    addToObserver(helloSub.value as HTMLElement)
+})
 </script>
 
 <template>
     <header class="hello" ref="el">
-        <svg class="hello__svg" viewBox="0 0 1297.56 500.59">
+        <svg class="hello__svg" viewBox="0 0 1297.56 500.59" ref="hello">
             <use xlink:href="/vectors/hello.svg#hello"></use>
         </svg>
 
@@ -19,7 +23,7 @@ onMounted(() => addToObserver(el.value))
             <h1>Hello</h1>
         </div>
 
-        <p class="hello__sub">I am a front end engineer with a deep love for UX/UI design and 3D</p>
+        <p class="hello__sub" ref="helloSub">I am a front end engineer with a deep love for UX/UI design and 3D</p>
     </header>
 </template>
 
@@ -28,27 +32,43 @@ onMounted(() => addToObserver(el.value))
 @use '@styles/typo';
 @use '@styles/utils';
 
+// Animations
+@use './scss/s-hello.animations';
+@use 'scss/common-animations';
+
 .hello {
     width: 100vw;
     padding-top: 40vh;
     padding-bottom: 10vh;
 
-    //background-color: greenyellow;
-
     .hello__svg {
         width: 100%;
-        fill: var(--color-emphasize);
-        stroke: black; // var(--color-emphasize);
-        stroke-width: 5px;
-        stroke-linejoin: round;
+        fill: transparent;
 
-        @include utils.baseTransition(fill);
+        stroke: var(--color-emphasize);
+        stroke-width: 4px;
+        stroke-linejoin: round;
+        stroke-dasharray: 0 1500;
+
+        // Transition for theme change
+        @include utils.baseTransition(fill, stroke);
+
+        &.on-screen {
+            animation: k-hello-show 0.75s ease-out forwards;
+            animation-delay: 0.25s;
+        }
     }
 
     &__sub {
         margin-left: 12vw;
         padding: 1.3rem;
         @include typo.body(1.6rem, #{var(--color-emphasize)});
+
+        opacity: 0;
+
+        &.on-screen {
+            @include common-animations.use('fade-in', 0.75s, 0.1s);
+        }
     }
 
     @include utils.media('t') {
